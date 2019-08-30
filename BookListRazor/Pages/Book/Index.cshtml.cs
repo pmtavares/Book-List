d3b95237cbc141dc4a9f86d8ea4c924d9a9954ac
pmtavares <pedrotavares41@gmail.com>
2019-08-30 21:07:25 +0100

    Add project files.

---
+++ BookListRazor/Pages/Book/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.Book
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
