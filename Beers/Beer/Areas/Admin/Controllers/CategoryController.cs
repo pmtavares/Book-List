b6595f431bdeff806c92c0ce2b0a5314a7411aa5
pmtavares <pedrotavares41@gmail.com>
2019-09-07 12:48:20 +0100

    Finished Category

--- Beers/Beer/Areas/Admin/Controllers/CategoryController.cs
+++ Beers/Beer/Areas/Admin/Controllers/CategoryController.cs
@@ -66,10 +66,59 @@ namespace Beer.Areas.Admin.Controllers
 
 
         [HttpGet]
-        public IActionResult Details(int id)
+        public async Task<IActionResult> Details(int id)
         {
-            Category category = _db.Category.Find(id);
+            Category category = await _db.Category.FindAsync(id);
             return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Update(category);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        //GET Delete
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var category = await _db.Category.FindAsync(id);
+
+            if (category == null)
+            {
+                return View();
+            }               
+          
+            _db.Remove(category);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
--- Beers/Beer/Models/Category.cs
+++ Beers/Beer/Models/Category.cs
@@ -12,6 +12,7 @@ namespace Beer.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name="Category Name")]
         public string Name { get; set; }
 
 
