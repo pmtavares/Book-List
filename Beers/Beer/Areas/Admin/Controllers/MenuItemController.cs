26817af58c1e7a5c961d385438743095296d9296
pmtavares <pedrotavares41@gmail.com>
2019-09-11 21:01:07 +0100

    Added Menuitem Migration and Controller with Index

---
+++ Beers/Beer/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Beer.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Beer.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public MenuItemController(ApplicationDbContext db, IHostingEnvironment host)
+        {
+            _db = db;
+            _hostingEnvironment = host;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
+            return View(menuItems);
+        }
+    }
+}
--- Beers/Beer/Data/Migrations/20190911190550_addMenuItemToDb.Designer.cs
+++ Beers/Beer/Data/Migrations/20190911190550_addMenuItemToDb.Designer.cs
@@ -4,14 +4,16 @@ using Beer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Beer.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190911190550_addMenuItemToDb")]
+    partial class addMenuItemToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
