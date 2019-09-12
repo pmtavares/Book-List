d7350f26898ecd5386d4bbd68f5e8c737316b7a0
pmtavares <pedrotavares41@gmail.com>
2019-09-12 20:57:38 +0100

    MenuItem Create Page and added Tiny plugin

--- Beers/Beer/Areas/Admin/Controllers/MenuItemController.cs
+++ Beers/Beer/Areas/Admin/Controllers/MenuItemController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Beer.Data;
+using Beer.Models;
+using Beer.Models.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,15 +19,31 @@ namespace Beer.Areas.Admin.Controllers
         private readonly ApplicationDbContext _db;
         private readonly IHostingEnvironment _hostingEnvironment;
 
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+
         public MenuItemController(ApplicationDbContext db, IHostingEnvironment host)
         {
             _db = db;
             _hostingEnvironment = host;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new MenuItem()
+            };
+
         }
         public async Task<IActionResult> Index()
         {
             var menuItems = await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
             return View(menuItems);
+        }
+
+
+        //GET Create
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
         }
     }
 }
